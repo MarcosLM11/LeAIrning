5619fd003f45c1be420227e06bf63587c77dc50e
marcos <marcoslopezmarinn@gmail.com>
2025-09-30 09:21:07 +0200

    UsersService init

---
+++ UsersService/src/test/java/com/marcos/usersservice/UsersServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marcos.usersservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsersServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
